f1a453a142559ae3abd51f03ee64b57739f57843
cwj <643469910@qq.com>
2020-04-17 11:42:18 +0800

    lxh  app

--- src/main/java/com/lrj/service/impl/IOrderServiceImpl.java
+++ src/main/java/com/lrj/service/impl/IOrderServiceImpl.java
@@ -2,7 +2,9 @@ package com.lrj.service.impl;
 
 import com.lrj.VO.ConsigneeVo;
 import com.lrj.VO.OrderVo;
+import com.lrj.VO.Order_houseServiceVo;
 import com.lrj.VO.UserInfoVo;
+import com.lrj.constant.Constant;
 import com.lrj.mapper.IOrderMapper;
 import com.lrj.mapper.IUserMapper;
 import com.lrj.service.IOrderService;
@@ -26,6 +28,20 @@ public class IOrderServiceImpl implements IOrderService{
     private IOrderMapper orderMapper;
 
     public Integer createOrder(OrderVo orderVo) {
+        //不同订单走不同通道
+        Integer orderType = orderVo.getOrderType();
+        switch (orderType){
+            //单项洗衣通道
+            case 1:
+            //月卡洗衣
+            case 2:
+            //单项家政服务
+            case 3:
+                Order_houseServiceVo houseServiceOrder = new Order_houseServiceVo();
+                houseServiceOrder.setIsLock(Constant.UNLOCK);
+            //定制家政服务
+            case 4:
+        }
         return orderMapper.createOrder(orderVo);
     }
 
