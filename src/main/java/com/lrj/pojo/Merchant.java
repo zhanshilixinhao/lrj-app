10123ee4b55542d32777cd668fd1a794f03813d8
cwj <643469910@qq.com>
2020-07-20 15:21:19 +0800

    APP 修正

--- src/main/java/com/lrj/pojo/Merchant.java
+++ src/main/java/com/lrj/pojo/Merchant.java
@@ -5,18 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-<<<<<<< HEAD
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.util.Date;
-=======
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
->>>>>>> origin/master
 
 /**
  * @Description: 商家管理类
@@ -27,19 +24,12 @@ import java.math.BigDecimal;
 @Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
-<<<<<<< HEAD
-@Table(name = "merchant")
-public class Merchant{
-    private static final long serialVersionUID = 2607786623630077533L;
-    @Id
-=======
 
 @Table(name = "merchant")
 public class Merchant extends Base {
     private static final long serialVersionUID = 2607786623630077533L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
->>>>>>> origin/master
     private Integer merchantId;
     private String name;
     private Byte type;
@@ -53,7 +43,6 @@ public class Merchant extends Base {
     /**提成比例*/
     private BigDecimal commissionRatio;
     private Byte active;
-<<<<<<< HEAD
     private Date createTime;
     private Date updateTime;
     /**二维码*/
@@ -63,10 +52,6 @@ public class Merchant extends Base {
      private String saleEndTime;//销售结束时间
     /**商家可用于销售的订单Id*/
     private Integer merchantOrderId;//
-=======
-    /**二维码*/
-    private String qrCode;
->>>>>>> origin/master
 
     public static final String COLUMN_MERCHANT_ACTIVE = "active";
     public static final int ACTIVE_TRUE = 1;
